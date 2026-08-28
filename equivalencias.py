4a2c72b72b1d99c8da7f5726827520162d21d4ec
Esteban Corsino <teby.msp@gmail.com>
2026-08-28 01:18:49 -0600

    agregamos nombres de variables de diabetes

---
+++ equivalencias.py
@@ -0,0 +1,90 @@
+# ============================================================
+# Equivalencias entre las descripciones de las variables
+
+# Esta nomenclatura pertenece al dataset de Diabetes1
+diabetes1_dataset = {
+    # Nombre original HighBP
+    "PAA":[
+        " Presión arterial alta "
+    ],
+    # Nombre original HighChol
+    "CoAl":[
+        "Colesterol alto"
+    ],
+    # Nombre original CholCheck
+    "CC5A":[
+       "Control de colesterol realizado en los últimos 5 años"
+   ],
+    # Nombre original BMI
+    "IMC":[
+        "Índice de masa corporal"
+    ],
+    # Nombre original Smoker
+    "F100C":[
+     "¿Ha fumado al menos 100 cigarrillos en toda su vida? "  
+    ],
+    # Nombre original  HeartDiseaseorAttack
+    "ECI":[
+    " ¿Alguna vez le han dicho que tiene enfermedad coronaria o infarto de miocardio? "       
+    ],
+    # Nombre original PhysActivity
+    "ACF":[
+        "¿Ha realizado actividad física en los últimos 30 días, aparte de su trabajo habitual?"
+    ],
+    #  Nombre original Fruits
+    "CF":[
+        "¿Consume frutas al menos una vez al día?"
+    ],
+    # Nombre original Veggies
+    "CV":[
+        "¿Consume verduras al menos una vez al día?"
+    ],
+    # Nombre original HvyAlcoholConsump
+    "CAE":[
+        "Bebedores empedernidos (hombres adultos que han tomado más de 14 copas a la semana y mujeres adultas que han tomado más de 7 copas a la semana)"
+    ],
+    # Nombre original AnyHealthcare
+    "CoM":[
+        "¿Tiene algún tipo de cobertura de seguro médico, o tiene algún tipo de plan de pago para la atención médica?"
+    ],
+    # Nombre original NoDocbcCost
+    "SAMC":[
+        "¿Ha dejado de ir al médico en el último año debido a los costos?"
+    ],
+    # Nombre original GenHlth
+    "SaG":[
+        "¿Cómo calificaría su salud en general?"
+    ],
+    # Nombre original MentHlth
+    "DSMM30":[
+        "Ahora, pensando en su salud mental, ¿cuántos días durante los últimos 30 días su salud mental no fue buena?"
+    ],
+    # Nombre original PhysHlth
+    "DSFM30":[
+        "Ahora, pensando en su salud física, ¿cuántos días durante los últimos 30 días su salud física no fue buena?"
+    ],
+    # Nombre original DiffWalk
+    "DMCE":[
+        "¿Tiene alguna dificultad para caminar o subir escaleras?"
+    ],
+    # Nombre original Sex
+    "Sexo":[
+        "Sexo"
+    ],
+    # Nombre original Age
+    "Edad":[
+        " Categoría de edad de 13 niveles (1 = 18-24, 2 = 25-29, ..., 13 = 80 o más) "
+    ],
+    # Nombre original Education
+    "Educación":[
+        "Nivel de educación (1 = Ninguno, 2 = Primaria, 3 = Secundaria, 4 = Bachillerato, 5 = Universidad)"
+    ],
+    # Nombre original Income
+    "Ingresos":[
+        "Nivel de ingresos (1 = menos de $10,000, ..., 8 = $75,000 o más)"
+    ],
+    # Nombre original Diabetic
+    "DiDia":[
+        "¿Alguna vez le han dicho que tiene diabetes?"
+    ]
+}
