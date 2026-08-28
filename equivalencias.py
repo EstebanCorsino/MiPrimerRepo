42f639cdf7ea23a563073ae2a2c6418dff23e08d
Esteban Corsino <teby.msp@gmail.com>
2026-08-28 15:59:47 -0600

    agregamos las variables de Heart

--- equivalencias.py
+++ equivalencias.py
@@ -45,7 +45,7 @@ diabetes1_dataset = {
     ],
     # Nombre original HvyAlcoholConsump
     "CAE":[
-        "Bebedores empedernidos (hombres adultos que han tomado más de 14 copas a la semana y mujeres adultas que han tomado más de 7 copas a la semana)"
+        "Bebedores empedernidos"
     ],
     # Nombre original AnyHealthcare
     "CoM":[
@@ -60,15 +60,15 @@ diabetes1_dataset = {
         "¿Cómo calificaría su salud en general?"
     ],
     # Nombre original MentHlth
-    "DSMM30":[
+    "SaludMental":[
         "Ahora, pensando en su salud mental, ¿cuántos días durante los últimos 30 días su salud mental no fue buena?"
     ],
     # Nombre original PhysHlth
-    "DSFM30":[
+    "SaluudFisica":[
         "Ahora, pensando en su salud física, ¿cuántos días durante los últimos 30 días su salud física no fue buena?"
     ],
     # Nombre original DiffWalk
-    "DMCE":[
+    "DCE":[
         "¿Tiene alguna dificultad para caminar o subir escaleras?"
     ],
     # Nombre original Sex
@@ -81,11 +81,11 @@ diabetes1_dataset = {
     ],
     # Nombre original Education
     "Educación":[
-        "Nivel de educación (1 = Ninguno, 2 = Primaria, 3 = Secundaria, 4 = Bachillerato, 5 = Universidad)"
+        "Nivel de educación"
     ],
     # Nombre original Income
     "Ingresos":[
-        "Nivel de ingresos (1 = menos de $10,000, ..., 8 = $75,000 o más)"
+        "Nivel de ingresos"
     ],
     # Nombre original Diabetic
     "DiDia":[
@@ -196,3 +196,308 @@ Kidney1_dataset = {
         "Clase)"
     ]
 }
+
+# Esta nomenclatura pertenece al dataset de Heart Disease
+Heart1_dataset = {
+    # Nombre original id
+    "ID":[
+        "Identificador del paciente"
+    ],
+    # Nombre original age
+    "Edad":[
+        "Edad del paciente en dias"
+    ],
+    # Nombre original Gender
+    "Sexo":[
+        "Sexo del paciente"
+    ],
+    # Nombre original Height
+    "Altura":[
+        "Altura del paciente en cm"
+    ],
+    # Nombre original Weight
+    "Peso":[
+        "Peso del paciente en kg"
+    ],
+    # Nombre original ap_hi
+    "PAS":[
+        "Presion arterial sistolica"
+    ],
+    # Nombre original ap_lo
+    "PAD":[
+        "Presion arterial diastolica"
+    ],
+    # Nombre original Cholesterol
+    "Col":[
+        "Colesterol del paciente "
+    ],
+    # Nombre original Glucose
+    "Glucosa":[
+        "Nivel Glucosa"
+    ],
+    # Nombre original Smoke
+    "Fumador":[
+        "Indica si el paciente fuma"
+    ],
+    # Nombre original alco
+    "Alcohol":[
+        "Indica si el paciente consume alcohol"
+    ],
+    # Nombre original Active
+    "Activo":[
+        "Indica si el paciente realiza actividad fisica"
+    ],
+    # Nombre original Cardio
+    "Cardiopatia":[
+        "Indica si el paciente tiene enfermedad cardiovascular"
+    ]
+}
+
+Heart2_dataset = {
+    # Nombre original age
+    "Edad":[
+        "Edad en anios"
+    ],
+    # Nombre original sex
+    "Sexo":[
+        "Sexo del paciente"
+    ],
+    # Nombre original cp
+    "TDT":[
+        "Tipo de dolor toracico"
+    ],
+    # Nombre original trestbps
+    "PAR":[
+        "Presion arterial en reposo"
+    ],
+    # Nombre original chol
+    "Col":[
+        "Colesterol serico en mg/dl"
+    ],
+    # Nombre original fbs
+    "ASA":[
+        "Nivel de glucosa en sangre en ayunas"
+    ],
+    # Nombre original restecg
+    "EER":[
+        "Electrocardiograma en reposo"
+    ],
+    # Nombre original thalach
+    "FCM":[
+        "Frecuencia cardiaca maxima"
+    ],
+    # Nombre original exang
+    "AIE":[
+        "Indica si el paciente tiene angina inducida por ejercicio"
+    ],
+    # Nombre original oldpeak
+    "DIpE":[
+        "Depresion del segmento ST inducida por el ejercicio"
+    ],
+    # Nombre original slope
+    "Pendiente":[
+        "Pendiente del segmento ST"
+    ],
+    # Nombre original ca
+    "CA":[
+        "Numero de vasos principales (0-3) coloreados por fluoroscopia"
+    ],
+    # Nombre original thal
+    "TAL":[
+        "Resultado de la prueba de estres con talio"
+    ],
+    # Nombre original num
+    "Cardiopatia":[
+        "Diagnostico de enfermedad cardiaca"
+    ]
+}
+
+Heart3_dataset = {
+    # Nombre original patientind
+    "ID":[
+        "Identificador del paciente"
+    ],
+    # Nombre original age
+    "Edad":[
+        "Edad del paciente (anios)"
+    ],
+    # Nombre original gender
+    "Sexo":[
+        "Sexo del paciente"
+    ],
+    # Nombre original chestpain
+    "TDT":[
+        "Tipo de dolor en el pecho"
+    ],
+    # Nombre original restingBP
+    "PAR":[
+        "Presion arterial en reposo"
+    ],
+    # Nombre original serumchlestrol
+    "ColS":[
+        "Colesterol serico en mg/dl"
+    ],
+    # Nombre original fastingbooldsugar
+    "ASA":[
+        "Azucar en sangre en ayunas"
+    ],
+    # Nombre original restingelectro
+    "RER":[
+        "Resultados Electrocardiograficos en reposo"
+    ],
+    # Nombre original maxheartrate
+    "FCM":[
+        "Frecuencia cardiaca maxima alcanzada"
+    ],
+    # Nombre original exerciseangia
+    "AIE":[
+        "Indica si el paciente tiene angina inducida por ejercicio"
+    ],
+    # Nombre original oldpeak
+    "DIpE":[
+        "Depresion del ST inducida por el ejercicio en relacion con el reposo"
+    ],
+    # Nombre original slope
+    "Pendiente":[
+        "Pendiente del segmento ST"
+    ],
+    # Nombre original noofmajorvessels
+    "CA":[
+        "Numero de vasos principales (0-3) coloreados por fluoroscopia"
+    ],
+    # Nombre original target
+    "Cardiopatia":[
+        "Presencia de enfermedad cardiaca"
+    ]
+}
+
+Heart4_dataset = {
+    # Nombre original age
+    "Edad":[
+        "Edad del paciente (anios)"
+    ],
+    # Nombre original anaemia
+    "Anaemia":[
+        "Disminucion de globulos rojos o hemoglobina"
+    ],
+    # Nombre original creatinine_phosphokinase (cpk)
+    "CPK":[
+        "Nivel de la enzima CPK en sangre"
+    ],
+    # Nombre original diabetes
+    "Diabetes":[
+        "Si el paciente tiene diabetes"
+    ],
+    # Nombre original ejection_fraction
+    "PSCC":[
+        "Porcentaje de sangre que sale del corazón en cada contracción (porcentaje)"
+    ],
+    # Nombre original high_blood_pressure
+    "HTA":[
+        "Indica si el paciente tiene hipertencion"
+    ],
+    # Nombre original platelets
+    "Plaquetas":[
+        "Plaquetas en la sangre"
+    ],
+    # Nombre original Sex
+    "Sexo":[
+        "Sexo del paciente"
+    ],
+    # Nombre original serum_creatinine
+    "Creatinina":[
+        "Nivel de creatinina serica en sangre"
+    ],
+    # Nombre original serum_sodium
+    "Sodio":[
+        "Nivel de sodio en sangre"
+    ],
+    # Nombre original smoking
+    "Fumador":[
+        "Indica si el paciente fuma"
+    ],
+    # Nombre original time
+    "Tiempo":[
+        "Tiempo de seguimiento del paciente"
+    ],
+    # Nombre original death_event
+    "EventoMuerte":[
+        "Si el paciente fallecio durante el periodo de seguimiento"
+    ]
+}
+
+Heart5_dataset = {
+    # Nombre original HeartDisease
+    "Cardiopatia":[
+        "Indica si el paciente tiene enfermedad cardiaca"
+    ],
+    # Nombre original BMI
+    "IMC":[
+        "Indice de masa corporal del paciente"
+    ],
+    # Nombre original Smoking
+    "Fumador":[
+        "Indica si el paciente a fumado al menos5 paquete de cigarrillos en su vida"
+    ],
+    # Nombre original AlcoholDrinking
+    "Alcohol":[
+        "Indica si el paciente es un bebedor excesivo de alcohol"
+    ],
+    # Nombre original Stroke
+    "ADC":[
+        "Indica si el paciente ha tenido un derrame cerebral"
+    ],
+    # Nombre original PhysicalHealth
+    "SaludFisica":[
+        "Numero de dias en los ultimos 30 dias en que la salud fisica del paciente no fue buena"
+    ],
+    # Nombre original MentalHealth
+    "SaludMental":[
+        "Numero de dias en los ultimos 30 dias en que la salud mental del paciente no fue buena"
+    ],
+    # Nombre original DiffWalking
+    "DCE":[
+        "Indica si el paciente tiene dificultad para caminar o subir escaleras"
+    ],
+    # Nombre original Sex
+    "Sexo":[
+        "Sexo del paciente"
+    ],
+    # Nombre original AgeCategory
+    "Edad":[
+        "Categoria de edad del paciente"
+    ],
+    # Nombre original Race
+    "Raza":[
+        "Raza del paciente"
+    ],
+    # Nombre original Diabetic
+    "Diabetes":[
+        "Indica si el paciente tiene diabetes"
+    ],
+    # Nombre original PhysicalActivity
+    "ActividadFisica":[
+        "Indica si el paciente realiza actividad fisica en los ultimos 30 dias"
+    ],
+    # Nombre original GenHealth
+    "SaludGeneral":[
+        "Nivel de salud general percibida por el paciente"
+    ],
+    # Nombre original SleepTime
+    "TiempoSueño":[
+        "Horas de suenio promedio del paciente"
+    ],
+    # Nombre original Asthma
+    "Asma":[
+        "Indica si el paciente tiene asma"
+    ],
+    # Nombre original KidneyDisease
+    "EnfermedadRenal":[
+        "Indica si el paciente tiene enfermedad renal"
+    ],
+    # Nombre original SkinCancer
+    "CancerPiel":[
+        "Indica si el paciente tiene cancer de piel"
+    ]
+}
+
